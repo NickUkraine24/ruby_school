d88360018bfe1317b4e82dbe7a6fcffdefb4f52f
reva_mariia <revafedenkom@gmail.com>
2021-11-16 18:17:18 +0200

    added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,13 @@
+puts 'Hello World!'
+a = 1
+puts a
+print 'Hello World!'
+print "\nHello World!\n"
+p 'Hello!'
+p [1, 2, 'hello']
+p 1.class
+p 0.95.class
+p true.class
+p false.class
+p 'false'.class
+p [1, 2, 3].class
