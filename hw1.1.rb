92f763caf3a5d883e306c7585a4143bb4a9c2bd1
Liudmyla <hrytsiuk.liudmyla@gmail.com>
2021-11-17 14:22:50 +0200

    Added first commit for hw1.1

---
+++ hw1.1.rb
@@ -0,0 +1,5 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you. What is your last name?"
+last_name = gets.chomp
+puts "Great. So your full name is " + first_name + " " + last_name
