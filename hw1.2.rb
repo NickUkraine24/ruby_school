eca1de322cf8b6b4f5604e578bd2ab73022fcd64
kkhoma <kkhoma@softserveinc.com>
2021-11-20 21:47:10 +0200

    Commiting hw2

---
+++ hw1.2.rb
@@ -0,0 +1,53 @@
+#Example of integer
+int = 12
+puts int
+
+#Example of boolean
+boolean = false
+puts boolean
+
+#Example of string
+string = 'Ruby is cool'
+puts string
+
+#Example of symbol
+symbol = :somesymbol
+puts symbol
+
+#Example of array
+array = ['Hoverla', 'Petros', 'Hytun-Temnatyk', 'Rebra', 'Brebeneskul', 'Pip-Ivan']
+puts array
+
+#Example of hashes
+hashes = {mountain: 'Hoverla', lake: 'Brebeneskul', mountainridge: 'Chornohora'}
+puts hashes
+
+#Step3 Variables
+apple = 130
+prune = 170
+fruits = apple + prune
+puts fruits
+
+#Step 4 Empty hashes and arrays
+emptyhashh1 = {}
+emptyhash2 = Hash.new
+puts emptyhashh1
+
+emptyarray1 = []
+emptyarray2 = Array.new
+puts emptyarray2
+
+#Steps 5-8 Working with arrays
+food = ['bacon', 'orange', 'apple']
+puts food.first
+puts food.last
+puts food.push 'yogurt'
+
+#Step 9 Working with a hash
+countrycode = {France: 'FR', Spain: 'ES', GreatBritain: 'UK'}
+puts countrycode
+
+#Step 10 Working with strings
+string1 = 'course'
+puts string1.gsub('ourse', 'anada')
+puts string1.capitalize
