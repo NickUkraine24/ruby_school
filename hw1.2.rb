21ffbb82b96ba8d7161ead2ed2e7df358dda0e2e
SvitlanaButs <vinrova@gmail.com>
2021-11-20 14:14:36 +0200

    added first commit for hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,26 @@
+p 1994 #Integer
+p 1==1 #TrueClass
+p 1>2 #FalseClass
+p "Virtual Code" #String
+p b=:"Feeling good"# Symbol
+p ['Cat', 'Dog', 'Monkey', 'Pig'] #Array
+{ first: 'smile' , second: 'angry'} #Hash
+
+@apple = 130
+@prune = 170
+@fruits = @apple + @prune
+
+{}.empty?
+[].empty?
+
+food = ['Prune', 'Orange', 'Apple']
+['Prune', 'Orange', 'Apple'].first
+['Prune', 'Orange', 'Apple'].last
+['Prune', 'Orange', 'Apple'][-1]
+food << "Yogurt"
+
+{ IT:'Italia', FR:'France', DE:'Germany'}
+
+'course'.gsub('ourse', 'anada')
+country = 'canada'
+country.upcase
