217c754c80077b7ef603c4cdb1f1be7404d84434
Zelezn9k <5llayboy41k@gmail.com>
2021-11-20 20:13:38 +0200

    addet hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,34 @@
+puts 'Ex2'
+
+a = 13 #Int
+
+b = 1.13 #Float
+
+c = true #Boolean
+
+d = 'That nice' #String
+
+nickname = [ 'Vika', 'Kristina', 'Rosa', 'Kate', 'Mike', 'Anti-Mage'] #Array
+
+s = :what #symbol
+
+h = {name:'Dima',surname:'Zelezn9k',age:'23'} #Hash
+
+@apple = 130
+@prune = 170
+@fruits = @apple + @prune
+
+{}.empty?
+[].empty?
+
+food = ['Prune', 'Orange', 'Apple']
+['Prune', 'Orange', 'Apple'].first
+['Prune', 'Orange', 'Apple'].last
+['Prune', 'Orange', 'Apple'][-1]
+food << "Yogurt"
+
+{ UK:'Ukraine', PL:'Poland', CH:'China'}
+
+'upranc'.gsub('pranc', 'kraine')
+country = 'ukraine'
+country.upcase
