891c8cd1f5dbc45cc5c275e104ec42df1d501dfe
Juanchovy <nagrudnyy@gmail.com>
2021-11-20 21:11:58 +0200

    added hw1.2.

---
+++ hw1.2.rb
@@ -0,0 +1,39 @@
+print "Ej.2\n"
+a = 5 # integer
+b = 15.5 # float
+c = 'Ruby School' # string
+d = true # boolean
+e = ['Volvo', 'Mercedes', 'Audi', 'Renault'] # array
+f = :things
+g = { brand: 'Volvo', model: 'XC90', year: 2017 } # hash
+puts a, b, c, d, e, f, g
+
+puts "\nEx.3"
+apple = 130
+prune = 170
+fruits = apple + prune
+
+puts "\nEx.4"
+Array.new
+[]
+Hash.new
+{ hi: 'hola', yes: 'sí', no: 'no' }
+
+puts "\nEx.5"
+food = ['bacon', 'orange', 'apple']
+
+puts "\nEx.6"
+p food.first
+
+puts "\nEx.7"
+p food.last
+
+puts "\nEx.8"
+food << 'yogurt'
+
+puts "\nEx.9"
+cc = { ES: 'Spain', UA: 'Ukraine', RO: 'Romania' }
+
+puts "\nEx.10"
+idk = 'course'
+idk.gsub('ourse', 'anada').upcase
