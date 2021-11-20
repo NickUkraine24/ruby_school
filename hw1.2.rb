77f2a1f202d82c93f54579a6ec5532e91395d9fd
kapatsilka <m.kapatsila@gmail.com>
2021-11-20 10:29:45 +0200

    added commit for hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,39 @@
+# ex 1
+puts 13 # Integer
+puts false # Boolean
+puts "my string" # String
+puts :cat # Symbol
+p arr = [1,2,3] # Array
+p h = {name: 'Tom', animal: 'cat'} # Hash
+
+# ex 2
+apple = 130
+prune = 170
+p fruits = apple + prune
+
+# ex 3
+[] # Array 1
+Array.new # Array 2
+{} # Hash 1
+Hash.new # Hash 2
+
+# ex 4
+puts food = ['bacon', 'orange', 'apple']
+
+# ex 5
+puts food.first
+
+# ex 6
+puts food.last
+
+# ex 7
+p food.push('yogurt')
+
+# ex 8
+puts countries = {UA: 'Ukraine', ARM:'Armenia', MA: 'Marokko'}
+
+# ex 9
+p str = "course"
+p str.sub('ourse', 'anada')
+puts str.upcase
+
