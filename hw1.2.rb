e161c833e2a546ac00358e9f0cfed9c4e1455065
FakasAnna <dmitrivnaan@gmail.com>
2021-11-21 16:22:50 +0300

    added commit for hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,19 @@
+apple = 130
+
+prune = 170
+
+fruits = (130 + 170)
+
+food = ['bacon', 'orange', 'apple']
+
+['bacon', 'orange', 'apple'] .first
+
+['bacon', 'orange', 'apple'] .last
+
+food.push ('yogurt')
+
+{ FR: 'France', IT: 'Italy', UA: 'Ukraine' }
+
+'course'.gsub('ourse','anada')
+country = 'canada'
+country.upcase
