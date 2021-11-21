70988d93ec756024a3bbf4d7b3767f0f40968780
Pasha-vel <pashavel1982@gmail.com>
2021-11-21 18:24:45 +0200

    added hw1.1 and hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,46 @@
+#2
+int = 20
+puts int 
+string = 'reno'
+puts string
+boolean = false
+puts boolean
+symbol = :programmme
+puts symbol
+float = 2.5
+puts float
+array = ['volvo','fiat', 'audi']
+puts array
+hashes = {:name => 'Ivan', :age => 30, :hair => 'black'}
+puts hashes
+
+#3
+Array.new
+[]
+Hash.new
+{}
+puts Array.new
+puts Hash.new
+
+#4
+apple = 130
+prune = 170
+fruits = apple + prune
+puts fruits
+
+#5-8
+food = ['bacon','orange','apple']
+puts food
+puts food.first
+puts food.last
+puts food.push'yogurt'
+
+#9
+countries = Hash.new
+countries = {:GER => 'Germany', :ITA => 'Italy',:POL => 'Poland'}
+puts countries
+
+#10
+tema = 'course'
+puts tema.gsub('ourse','anada')
+puts tema.capitalize!
