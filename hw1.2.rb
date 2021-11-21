78d5e247ea85144cc803d64683518a9dc2d37126
oksenyuk-vi <vioksenyuk@gmail.com>
2021-11-21 13:54:04 +0200

    added commit for hw1.2.rb

---
+++ hw1.2.rb
@@ -0,0 +1,71 @@
+# 2
+# Example of Integer 
+x = 5
+puts x
+
+# Example of Boolean
+color = "red"
+if (color == "red")
+    puts true
+else
+    puts false
+end
+
+# Example of String
+hello = "Привіт, друзі!"
+puts hello
+
+# Example of Symbol 
+a = 15
+b = 25
+c = (a + b)/2
+puts "Перше число:" + a.to_s
+puts "Друге число:" + b.to_s
+puts "Среднє:" + c.to_s
+
+# Example of Array
+puts 'How is your day?'
+puts
+answers = ['good',
+ 'bad', 
+ 'not wery well', 
+ 'awessome', 
+ 'fine', 
+ 'terrible'] 
+puts answers.sample
+
+# Example of Hashes
+
+#3
+apple = 130
+prune = 170
+fruits = apple + prune
+puts fruits
+
+#4
+hash.new {}
+array.new []
+
+#5 
+array = ['bacon', 'orange', 'apple']
+puts array
+
+#6
+puts(array.first)
+
+#7
+puts (array.last)
+
+#8
+array.push ['yogurt']
+puts array
+
+#9
+hash = {UA: 'Ukraine', RU: 'Russia', UK: 'United Kingdom'}
+puts hash
+
+#10
+string = "course"
+puts string
+puts string.sub('ourse', 'anada')
+puts string.capitalize
