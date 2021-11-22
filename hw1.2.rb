7fd4537f19067f865c3d76570c4018b38f6dbce1
inahuliak <inahuliak01@gmail.com>
2021-11-22 13:54:35 +0200

    added homework in hw.1.2

---
+++ hw1.2.rb
@@ -0,0 +1,46 @@
+#2. Create variables by all types of Ruby (Integer, Boolean, String, Symbol, Array, and Hashes) and output these variables (p, puts, print)
+var1 = 20 
+var2 = true
+var3 = "Hello, my dear friends"
+var4 = :firstsymb
+var5 = [3, 6, 8, 9, 5]
+var6 = {name: 'hashhh'}
+
+puts var1, var2, var3, var4, var6
+print var5, "\n"
+
+#3. 
+apple = 130
+prune = 170
+fruits = apple + prune 
+
+#4. 
+hsh = { }
+arr = []
+
+{}
+[]
+
+#5.
+food = [ 'bacon', 'orange', 'apple']
+
+#6
+ food.first
+
+#7
+ food.last
+
+#8
+ food.append('yogurt')
+
+#9
+hcountry={code: 'AT', name: 'Austria'}
+hcountry={code: 'AL', name: 'Albania'}
+hcountry={code: 'BS', name: 'Bahamas'}
+
+#10
+'course'.gsub('ourse', 'anada')
+
+
+
+
