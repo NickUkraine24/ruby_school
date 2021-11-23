001f7cc9d167366e5ea4fc35b9a944371b26c2c9
Popova-Anast <94473154+Popova-Anast@users.noreply.github.com>
2021-11-23 21:49:19 +0200

    homework 1.2

---
+++ hw.1.2.rb
@@ -0,0 +1,73 @@
+# ex 1
+number = 2.3
+puts number.to_i
+
+t = 1
+f = 0
+puts t
+puts f
+
+# String
+s = String.new("ruby")
+puts s
+
+#Array
+ary = [1, 2, 3]
+puts ary
+
+#Hash
+person = { :name => 'Matz', :language =>'Ruby' }
+puts person[:name]
+puts person[:language]
+
+#ex2
+apple = 170
+prune = 130
+fruits = [prune, apple]
+puts fruits
+
+#ex 3
+ary = ['Apple', 'Banana', 'Pinaple']
+
+emptyArray = Array.new
+
+#hash
+{}
+eH2 = Hash.new
+
+#5,6,7
+food = ['bacon', 'orange', 'apple']
+puts food[-1]
+puts food[0]
+
+#8
+food = ['bacon', 'orange', 'apple']
+
+food.push('yougurt')
+puts food
+
+#9
+code = { Hungary: '+30', Netharlands: '+31', Belgium: '+32'}
+puts code
+
+#ex10
+str = ("course")
+str.gsub("course", "canada")
+
+puts str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
