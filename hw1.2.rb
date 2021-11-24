b161006d56c2e38bd731d9318a8cbe551fa0618d
Andrii-Dm <andy29229@gmail.com>
2021-11-24 21:23:12 +0200

    added first commit for hw1.2

---
+++ hw1.2.rb
@@ -0,0 +1,52 @@
+#1
+a =  5
+p(a)
+
+b = true
+puts(b)
+
+c = "Hello"
+puts(c)
+
+$d = :Andrew
+puts($d)
+
+e = [1, 2, 3, 4]
+puts(e)
+
+hash = Hash[f: 0, g: 1, j: 2]
+hash.each { |key, value| puts "key: #{key}, value: #{value}" }
+
+#2
+
+apple = 130
+prune = 170
+fruits = apple + prune
+puts fruits
+
+#3
+
+arr = []
+hash2 = Hash[]
+
+#4
+
+food = %w[bacon orange apple]
+puts(food[0])
+puts(food[1])
+puts(food[2])
+
+#5
+
+country_hash={ES:'Spain',UA:'Ukraine',US:'USA'}
+country_hash.each { |key, value| puts "key: #{key}, value: #{value}" }
+
+#6
+
+str="Course"
+puts(str)
+str["ourse"]="anada"
+puts(str)
+
+
+
