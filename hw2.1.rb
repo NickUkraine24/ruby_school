b14a067dfa8de56a758297981144b46f2983bcca
taras-chepil <taras12345a@gmail.com>
2021-11-27 20:51:04 +0200

    added firt commit fo hw2.2

--- hw2.1.rb
+++ hw2.1.rb
@@ -6,7 +6,7 @@
 #e) (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false #true
 
 #3. Write a conditional that check if age of the person more 18 and less 25 print that "he/she study at university" next if age of the person more or equal 25 and less or equal 60 print that "he/she has a job" and else that print "he/she needs to relax".
-age = 1
+age = 61
  if age  > 18 && age < 25
   puts "he/she study at university"
 elsif age >= 25 && age <= 60
@@ -16,7 +16,7 @@ else
 end
 
 #4. Write a first conditional with if..else as the previous exercise in the ternary operator.
-grade = age  > 18 && age < 25 ? puts("he/she study at university") : age >= 25 && age <= 60 ? puts("he/she has a job") : puts("he/she needs to relax")
+age > 18 && age < 25 ? puts("he/she study at university") : age >= 25 && age <= 60 ? puts("he/she has a job") : puts("he/she needs to relax")
 
 #5. Write case, when that if a word is fruit, print array with random 3 fruits, if vegetable, print array with random 3 vegetables and else print "we don't know this word".
 word = "fruit"
---
+++ hw2.2.rb
@@ -0,0 +1,41 @@
+
+# 2. Get the name of the hash person
+person = { name: 'Bob', position: 'web developer', hobbies: 'painting' }
+puts person [ :name ]
+
+# 4. What the difference between merge and merge!
+cat = {name: "Tommy"}
+weight = {weight: "2 kg"}
+cat_new = cat.merge(weight)
+# merge illustrate merge merges hashes once without overwriting them
+puts cat_new
+puts cat
+# marge! illustrate merge! overwrites hashes and saves changes
+cat.merge!(weight)
+puts cat
+
+# 5. multiline comment
+=begin
+ Change hobbies onto an array with 3 random hobbies using all possible ways.
+What the difference between merge and merge! and illustrate (in comments) this with two hashes like:
+cat = {name: "Tommy"}
+weight = {weight: "2 kg"}
+=end
+
+# 6. Check if we have Bob value in the person
+person.has_value?( 'Bob' ) #=>true
+
+# 7. Delete the position key from the person
+person.delete(:position)
+puts person
+
+# 8.Write exprssions using all Assignment Operators with these two variables:
+a = 9
+b = 3
+puts z = a + b # Equal operator
+puts a += b # Add AND
+puts a -= b # Subtract AND
+puts a *= b # Multiply AND
+puts a /= b # Divide AND
+puts a **= b #Exponent AND
+puts a %= b # Modulus AND
