a3daa55a1667241bec4499ca4da76563e6b657b9
Natalia Panakhyd <51032306+NatalyCoding@users.noreply.github.com>
2021-11-28 18:05:08 +0200

    Create hw2.1.rb

---
+++ hw2.1.rb
@@ -0,0 +1,33 @@
+# ex2
+a) false
+b) false
+c) false
+d) true
+e) true
+
+# ex3
+age = 20        # this is my variable value, without her undefined local variable or method 'age' 
+if age > 18 && age < 25
+print 'he/she study at university'
+elsif age => 25 && age <= 60
+print 'he/she has a job'
+else 
+print 'he/she needs to relax'
+end
+
+# ex4
+age > 18 && age < 25 ? (print 'he/she study at university') : (print 'he/she has a job')
+
+# ex5
+case word
+when 'fruit'
+print ['apple','lemon','bananas']
+when 'vegetable'
+print ['potato','onion','cucumber']
+else
+print ["we don't know this word" ]	
+end
+
+# ex6
+# Result of this condition 
+puts "Alright now!"
