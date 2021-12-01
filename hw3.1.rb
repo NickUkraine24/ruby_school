521ad9d3fb05cef719a73603077df8db8df74711
kukurudz-nadia <94442918+kukurudz-nadia@users.noreply.github.com>
2021-12-01 09:58:04 +0200

    Create hw3.1.rb

---
+++ hw3.1.rb
@@ -0,0 +1,25 @@
+#ex1
+file = File.open("test_file_hw_3.1.txt", 'r')
+file.read 
+file.close
+
+#ex2
+time = Time.now
+time.strftime("%Y-%m-%d %I:%M %p")
+
+#ex3
+begin
+ person = { name: 'Den', age: 23, position: 'mentor' }
+ raise 'This person is not developer' if person.key?(:position) && person.value?("mentor")
+ rescue Exception => e
+ puts e.message
+end
+
+#ex4
+begin
+ str = "I will pass exams"
+ str.mmmm
+ rescue => error
+ puts error.message
+ puts "This is StandardError" 
+end
