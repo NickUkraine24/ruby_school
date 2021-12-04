87efa620ac0f7c9c151ce230c6a95b3694f7fd20
taras-chepil <taras12345a@gmail.com>
2021-12-04 22:25:43 +0200

    added hw3.2

---
+++ hw3.2.rb
@@ -0,0 +1,31 @@
+#Task 1
+def greeting(name = 'John')
+  p  "Hello  #{name}"
+end
+greeting 'Taras'
+
+#Task 2
+def multiply(x , y)
+  multiplying = x * y
+  p multiplying
+end
+multiply 2 , 4
+
+#Task 3
+def square(number)
+  square_result = number * number
+  p square_result
+end
+square 2
+
+#Task 4
+def info_with_args( name, birthday, height, weight )
+  p "name #{name}, birthday #{birthday}, height #{height}, weight #{weight} "
+end
+info_with_args'Taras', 28.07 , 185 , 93
+
+#Task 5
+def info_with_kwargs( name: , birthday: , height: , weight: )
+  p "name #{name}, birthday #{birthday}, height #{height}, weight #{weight} "
+end
+info_with_kwargs name: 'Taras', birthday: '28.07.1990' , height: 185, weight: 93
