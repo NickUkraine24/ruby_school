cee3e2afb7051737bd6fc8942f9aa5027690846c
savka-olia <tokarchykolia@gmail.com>
2021-12-05 14:44:57 +0200

    hw3.2

--- hw3.1.rb
+++ hw3.1.rb
@@ -1,22 +1,34 @@
 #task 1
 file = File.open("test_file_hw_3.1.txt").each { |line| p line}
+=begin
+file = File.open("test_file_hw_3.1.txt", "w+")
+file.write("Hello world")
+file.rewind
+puts file.read
+file.close
+=end
 
 #task 2
-time = Time.new.strftime("%Y-%m-%d %H:%M %P")
+time = Time.now.strftime("%Y-%m-%d %I:%M %P")
 p time
 
 #task 3
+person = { name: 'Den', age: 23, position: "mentor" }
 begin
-    person = { name: 'Den', age: 23, position: "mentor" }
+    # if person[:position] == "mentor"
     raise "This person isn't developer" if person.key?(:position) && person.value?("mentor")
-rescue => e
-	p e
+rescue StandardError => e
+	p e.message
+    p e.class
 end
 
 #task 4
 begin
-    "string☺".mmm
-rescue
+  "string☺".mmm
+  # mmm
+rescue StandardError => e
     p "This is StandardError"
     p e.message
 end
+ 
+
---
+++ hw3.2.rb
@@ -0,0 +1,34 @@
+#Task1
+def greeting(name = "John")
+    p "Hello, #{name}"
+end
+
+greeting("Olia")
+
+#task2
+def multiply(a, b)
+    p a * b
+end
+
+multiply(12,18)
+
+#task3
+def square(num)
+    res = num * num
+    p res
+end
+
+square(2)
+
+#task4
+def info_with_args(name, birthday, height, weight)
+    p "name: #{name}, birthday: #{birthday}, height: #{height}, weight: #{weight}."
+end
+info_with_args("Olia", "January, 19", 167, 56)
+
+#task5
+def info_with_kwargs(name: "Olia", birthday: "January, 19", height: 167, weight: 56)
+    p "name: #{name}, birthday: #{birthday}, height: #{height}, weight: #{weight}."
+end
+
+info_with_kwargs
