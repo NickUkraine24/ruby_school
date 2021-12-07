98df5e15baa95cd01bc04aee72c4118a3cf86669
savka-olia <tokarchykolia@gmail.com>
2021-12-07 14:15:55 +0200

    hw.4.1

--- hw4.1.rb
+++ hw4.1.rb
@@ -25,17 +25,17 @@ rectangle.get_area(4,8)
 class Rectangle
     attr_reader :width, :height
 
-    def width=(width)
-        raise ArgumentError.new(
-          "Bla"
-        ) if width < 0
-        @width = width
-        end
-
     # def width=(width)
-    #     raise ("The width must contain a positive value!") if width < 0
+    #     raise ArgumentError.new(
+    #       "The width must contain a positive value!"
+    #     ) if width < 0
     #     @width = width
-    # end
+    #     end
+
+    def width=(width)
+        raise ("The width must contain a positive value!") if width < 0
+        @width = width
+    end
 
     def height=(height)
         raise ("The height must contain a positive value!") if height < 0
@@ -49,6 +49,6 @@ class Rectangle
 end
 
 rectangle = Rectangle.new
-rectangle.width = -4
-rectangle.height = 4
+rectangle.width = 4
+rectangle.height = -4
 rectangle.get_area
