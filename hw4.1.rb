cd3779ea20b4b29978ec1bc58c1d8154668ce5bd
valentyna-oksenyuk <vioksenyuk@gmail.com>
2021-12-08 14:51:29 +0200

    added hw4.1.rb

---
+++ hw4.1.rb
@@ -0,0 +1,52 @@
+#1 Create a Cat class. Add a meow method to the Cat class that prints "I'm a cat, gimme food & pet me now!".
+# Create a cat object and call the meow method 3 times.
+class Cat
+ def Cat.meow
+   puts "I'm a cat, gimme food & pet me now!"
+ end
+end
+cat = Cat.new
+3.times{Cat.meow}
+
+#2 Create a Rectangle class. Add width and height attributes, with methods to read and write each.
+# Then add an area method that returns the area of the rectangle
+# (You can calculate this by multiplying the width attribute by the height attribute).
+  class Rectangle
+    attr_reader :width, :height
+    attr_writer :width, :height
+    def area
+      width * height
+    end
+  end
+  rectangle = Rectangle.new
+  rectangle.width = 12
+  rectangle.height = 20
+  p "Rectangle_area = #{rectangle.area}"
+
+  #3 In your Rectangle class in the second exercise, modify both the width= and height= attribute writer methods
+  # to raise an error if a negative value is provided.
+class Rectangle
+  attr_reader :width, :height
+  attr_writer :width, :height
+  def area
+    width * height
+  end
+
+  def width=(value)
+    @value = value
+    if value <= 0
+      raise "Error: a negative value is provided"
+    end
+  end
+  def height=(value)
+    @value = value
+    if value <= 0
+      raise "Error: a negative value is provided"
+    end
+  end
+
+rectangle = Rectangle.new
+rectangle.width = -12
+rectangle.height = 20
+p "Rectangle_area = #{rectangle.area}"
+
