75ce736b5bab5900d128ae54a9c571c9e645abd6
kukurudz-nadia <94442918+kukurudz-nadia@users.noreply.github.com>
2021-12-08 15:56:33 +0200

    Create hw4.1.rb

---
+++ hw4.1.rb
@@ -0,0 +1,47 @@
+#ex1
+class Cat
+  def meow
+     puts "I'm a cat, gimme food & pet me now!"
+  end
+end
+
+new_cat = Cat.new
+new_cat.meow
+new_cat.meow
+new_cat.meow
+
+#ex2
+class Rectangle
+  attr_accessor :width, :height
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+  def area
+    puts @width*@height
+  end
+end
+
+new_rectangle = Rectangle.new(20, 4)
+new_rectangle.area
+
+#ex3
+class Rectangle
+  attr_accessor :width, :height
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+  def area
+    begin
+     raise 'You mustn`t put negative numbers!' if @width < 0 || @height < 0
+     rescue Exception => e
+     puts e.message
+    end
+    puts @width*@height
+  end
+end
+
+new_rectangle = Rectangle.new(-9, 4)
+new_rectangle.area
+
