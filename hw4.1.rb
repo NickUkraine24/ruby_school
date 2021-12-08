749e10c8e011b618c4d235ac2e799d92bc44ae22
VKristina <kristinavepryk@gmail.com>
2021-12-08 16:34:41 +0200

    create hw4.1

--- hw4.1.rb
+++ hw4.1.rb
@@ -4,3 +4,26 @@ class Cat
         puts "I'm a cat, gimme food & pet me now!"
     end
 end
+cat = Cat.new
+3.times { puts "I'm a cat, gimme food & pet me now!" }
+
+#ex2
+class Rectangle
+    attr_accessor :width, :height
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  def area
+    puts @width * @height
+  end  
+end    
+
+rectangle = Rectangle.new
+rectangle.width = 8
+rectangle.height = 6
+puts rectangle.area
+
+#ex3
