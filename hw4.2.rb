0c808fa7b66f89f7d8fbcadefb10c3982e8dad5a
Pavlo Rymarchuk <lucisofi999@gmail.com>
2021-12-10 17:22:48 +0200

    add hw4.2

---
+++ hw4.2.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# ex1
+input_email = 'test1_2.course@example.com'
+
+if input_email.match(/\w+\d+.\w+@\w+.\w+$/)
+    puts "This #{input_email} is valid"
+end
+
+
+# ex2
+input_number = '+38(050)123-1344'
+                      
+if input_number.match(/^\+\d+\(\d+\)\d+\-\d+$/)
+    
+    puts "This #{input_number} is valid"
+end
