8b3a68ca41a0cc2faec5cf74b94c27f7b0f2f109
inahuliak <inahuliak01@gmail.com>
2021-12-10 18:13:50 +0200

    added file hw.4.2

---
+++ hw4.2.rb
@@ -0,0 +1,10 @@
+#1
+email = 'test1_2.course@example.com'.match(/[A-Za-z]\w+\d+.\w+@\w+.\w+/)
+if email 
+	puts "This #{email} is valid" 
+end
+
+#2
+if "+38(050)123-1344".match(/\+\d{2}\(\d{3}\)\d{3}-\d{4}/)
+    puts "This +38(050)123-1344 is valid"
+end
