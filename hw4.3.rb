8af84b75fe2e159fbe9e6ab0c508186f17b1d625
kapatsilka <m.kapatsila@gmail.com>
2021-12-11 21:17:07 +0200

    commit for hw4.3

---
+++ hw4.3.rb
@@ -0,0 +1,67 @@
+# hw4.3
+# ex1
+
+class Confection
+  def prepare
+    puts "Baking at 350 degrees for 25 minutes."
+  end
+end
+
+class Cupcake < Confection
+  def prepare
+    super
+    puts "Applying to frost"
+  end
+end
+
+cupcake = Cupcake.new
+cupcake.prepare
+
+# ex 2
+
+class Rectangle
+
+  attr_reader :width, :height
+
+  def width=(width)
+    if width < 0
+      raise "Error, width is negative"
+    end
+
+    @width = width
+  end
+
+  def height=(height)
+    if height < 0
+      raise "Error, height is negative"
+    end
+
+    @height = height
+  end
+
+  def area
+    width*height
+  end
+
+end
+
+class Square < Rectangle
+
+  def width=(width)
+    super
+    @width = width
+    @height = height
+  end
+
+  def height=(height)
+    super
+    @width = width
+    @height = height
+  end
+
+end
+
+square = Square.new
+square.width = 9
+square.height = 13
+p square.area
